da23a2e93a9ad24de7a0f2904451eaeaf6d909c0
zaru <zarutofu@gmail.com>
2025-01-25 11:55:13 +0900

    Initial commit

---
+++ test/cdk-signed-url-cloudfront.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkSignedUrlCloudfront from '../lib/cdk-signed-url-cloudfront-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-signed-url-cloudfront-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkSignedUrlCloudfront.CdkSignedUrlCloudfrontStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
