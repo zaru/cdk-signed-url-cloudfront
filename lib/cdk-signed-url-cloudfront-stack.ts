feb2696022262964d3d22b997067406891ec8c57
zaru <zarutofu@gmail.com>
2025-01-25 11:57:14 +0900

    add biome and fix

--- lib/cdk-signed-url-cloudfront-stack.ts
+++ lib/cdk-signed-url-cloudfront-stack.ts
@@ -1,16 +1,5 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class CdkSignedUrlCloudfrontStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkSignedUrlCloudfrontQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
-}
+export class CdkSignedUrlCloudfrontStack extends cdk.Stack {}
